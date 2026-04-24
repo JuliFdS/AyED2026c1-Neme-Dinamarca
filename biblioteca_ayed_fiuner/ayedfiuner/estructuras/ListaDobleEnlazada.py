4e2fb0a081e388e93e5c24a8e69d6fc2a3787da9
dndinamarca <daiana.dinamarca@ingenieria.uner.edu.ar>
2026-04-24 15:12:44 -0300

    implemento esta_vacia

---
+++ biblioteca_ayed_fiuner/ayedfiuner/estructuras/ListaDobleEnlazada.py
@@ -0,0 +1,12 @@
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+        self.anterior = None
+
+class ListaDobleEnlazada:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+        self.tamanio = 0
+
